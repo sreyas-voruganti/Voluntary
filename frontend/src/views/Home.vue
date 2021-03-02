fc5b7f88ecf2b28302f10cdf7e9533457abd79f4
Sreyas Voruganti <sreyas.voruganti@gmail.com>
2021-03-01 20:54:42 -0500

    width fix two

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -29,7 +29,7 @@
       </p>
     </div>
     <div class="container my-5 is-flex mobile-cont p-3" v-if="featured_service">
-      <div class="container">
+      <div class="mobile-feat-two container">
         <p class="title is-4 my-3">
           <i class="fas fa-award"></i> Featured Service:
         </p>
@@ -156,6 +156,9 @@ export default {
 }
 @media (min-width: 600px) {
   .mobile-popular {
+    width: 55%;
+  }
+  .mobile-feat-two {
     width: 45%;
   }
 }
