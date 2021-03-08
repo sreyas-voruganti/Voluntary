452337286cf6597cc23ad232c048c6b9da6c8fa8
Sreyas Voruganti <sreyas.voruganti@gmail.com>
2021-03-08 14:31:57 -0500

    add info abt env vars

---
+++ frontend_config.js
@@ -0,0 +1,6 @@
+// The environmental variables for the frontend, this file should be located in the frontend directory and named config.js
+
+export default {
+  apiBaseUrl: "the base url of your api",
+  socketBaseUrl: "the base url of your socket",
+};
